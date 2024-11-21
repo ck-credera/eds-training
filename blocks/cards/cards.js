aa0c125d2f6c1e8af98f6c029a796de0acf9554a
Chandan <team.hindustani@mercedes-benz.ca>
2024-11-21 17:44:33 +0530

    updated card.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -5,12 +5,15 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    li.className= 'card-item';
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else {
-      div.className = 'cards-card-body';
-     
+      else{
+        div.className = 'cards-card-body';
+        const document.createElement('p');
+        p.append(new Date().toDateString());
+        div.prepend (p):
       }
     });
     ul.append(li);
