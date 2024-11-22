d0fdebf81509a88d5a9e4c1931f158de17d088a9
Chandan <team.hindustani@mercedes-benz.ca>
2024-11-22 13:50:55 +0530

    Added promonav js css

---
+++ blocks/navpromo/navpromo.js
@@ -0,0 +1,19 @@
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+
+  // setup image columns
+  [...block.children].forEach((row) => {
+    row.classList.add('container');
+    [...row.children].forEach((col) => {
+      const ul = col.querySelector('ul');
+      if (ul) {
+        col.classList.add('list-section');
+        ul.classList.add('overlap-list');
+      }
+      else{
+        col.classList.add('display-section');
+      }
+    });
+  });
+}
