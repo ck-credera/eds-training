ecf798b3e7b223489e234aa92b53953e7070fa91
Chandan <team.hindustani@mercedes-benz.ca>
2024-11-22 13:53:14 +0530

    updated promonav.js

--- blocks/navpromo/navpromo.js
+++ blocks/navpromo/navpromo.js
@@ -10,8 +10,8 @@ export default function decorate(block) {
       if (ul) {
         col.classList.add('list-section');
         ul.classList.add('overlap-list');
-      }
-      else{
+        }
+      else {
         col.classList.add('display-section');
       }
     });
